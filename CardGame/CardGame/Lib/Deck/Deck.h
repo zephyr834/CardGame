766cb4c46b148cac1c286fd216f606b8fe21e3af
Cory Eighan <zephyr834@gmail.com>
2014-11-15 21:10:00 -0800

    Reorganized folders and added Deck class

---
+++ CardGame/CardGame/Lib/Deck/Deck.h
@@ -0,0 +1,13 @@
+//
+//  Deck.h
+//  CardGame
+//
+//  Created by Ninja Panda on 11/15/14.
+//  Copyright (c) 2014 Ninja Panda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Deck : NSObject
+
+@end
---
+++ CardGame/CardGame/Lib/Deck/Deck.m
@@ -0,0 +1,13 @@
+//
+//  Deck.m
+//  CardGame
+//
+//  Created by Ninja Panda on 11/15/14.
+//  Copyright (c) 2014 Ninja Panda. All rights reserved.
+//
+
+#import "Deck.h"
+
+@implementation Deck
+
+@end
