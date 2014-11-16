29b0db310f5b7d9f25ab531d6c6fb4366cf7bba7
Cory Eighan <zephyr834@gmail.com>
2014-11-15 20:08:44 -0800

    Initial commit and initial project creation.

---
+++ CardGame/CardGame/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CardGame
+//
+//  Created by Ninja Panda on 11/15/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
