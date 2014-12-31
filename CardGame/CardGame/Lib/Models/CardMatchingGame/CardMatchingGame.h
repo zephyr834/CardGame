d58c6382c698fc23db7afafe53da097586ff20c7
Cory Eighan <zephyr834@gmail.com>
2014-12-31 13:15:08 -0800

    Copied/added new deck buttons

---
+++ CardGame/CardGame/Lib/Models/CardMatchingGame/CardMatchingGame.h
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.h
+//  CardGame
+//
+//  Created by Ninja Panda on 12/31/14.
+//  Copyright (c) 2014 Ninja Panda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CardMatchingGame : NSObject
+
+@end
---
+++ CardGame/CardGame/Lib/Models/CardMatchingGame/CardMatchingGame.m
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.m
+//  CardGame
+//
+//  Created by Ninja Panda on 12/31/14.
+//  Copyright (c) 2014 Ninja Panda. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@implementation CardMatchingGame
+
+@end
